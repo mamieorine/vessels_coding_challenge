da968386d94b9b726c3c3622bfb8ec2153c938eb
mamieorine <mamieorine@gmail.com>
2022-10-08 18:43:56 +0100

    update put and delete methods

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import VesselApiView
+from api.views import VesselApiView, VesselDetailApiView
 
 urlpatterns = [
     path('vessel/', VesselApiView.as_view()),
+    path('vessel/<str:name>/', VesselDetailApiView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,14 +5,41 @@ from .models import Vessel
 from .serializers import VesselSerializer
 
 
+class VesselDetailApiView(APIView):
+    def put(self, request, name, *args, **kwargs):
+        vessel = Vessel.objects.get(name=name)
+        serializer = VesselSerializer(
+            data=request.data, instance=vessel, many=False)
+
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        instance = self.get_object(name)
+        if not instance:
+            return Response(
+                {"res": "Object with vessel id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, name, *args, **kwargs):
+        try:
+            vessel = Vessel.objects.get(name=name)
+        except Vessel.DoesNotExist:
+            return Response(
+                {"res": "Object with vessel does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+        vessel.delete()
+        return Response({"res": "Vessel deleted!"}, status=status.HTTP_200_OK)
+
+
 class VesselApiView(APIView):
     def get(self, request, *args, **kwargs):
-
-        if (Vessel.DoesNotExist):
-            return Response(
-                {"res": "Object with todo id does not exists"},
-                status=status.HTTP_400_BAD_REQUEST)
-
         serializer = VesselSerializer(Vessel.objects.all(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
